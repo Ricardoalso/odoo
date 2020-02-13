f30caa21dc48af80e72b47d16e48763d61d5ca15
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2020-02-13 18:49:36 +0100

    l10n_ch: define a specific grouping rule for ISR payments
    Swiss ISR payments must not be grouped as we want to keep a single transaction
    per reference. The ISR payment reference doesn't imply additionnal fees from
    the bank.
    
    One transaction per ISR reference is needed in Swiss SEPA payments
    to ensure a end-to-end flow that will ease the reconciliation on
    the other end.
    
    When ISR reference is detected we also avoid to concatenate the references
    to not concatenate the same reference multiple times. In case the
    same reference is sent by the supplier on different dates.
    
    Backport from PR 45744

--- addons/l10n_ch/models/res_bank.py
+++ addons/l10n_ch/models/res_bank.py
@@ -22,6 +22,20 @@ def _is_l10n_ch_postal(account_ref):
         return mod10r(account_ref_without_check) == account_ref
     return False
 
+def _is_l10n_ch_qr_iban(account_ref):
+    """Returns if the account_ref is a QR IBAN
+
+    A QR IBAN contains an IID QR.
+    An IID QR is between 30000 and 31999
+    It starts at the 5th character
+    eg: CH21 3080 8001 2345 6782 7
+    where 30808 is the IID QR
+    """
+    account_ref = account_ref.replace(' ', '')
+    return (account_ref.startswith('CH')
+            and account_ref[4:9] >= '30000'
+            and account_ref[4:9] <= '31999')
+
 
 class ResBank(models.Model):
     _inherit = 'res.bank'
@@ -50,6 +64,17 @@ class ResPartnerBank(models.Model):
             return 'postal'
         else:
             return super(ResPartnerBank, self).retrieve_acc_type(acc_number)
+
+    def is_isr_issuer(self):
+        """Supplier will provide ISR reference numbers in two cases:
+
+        - postal account number starting by 01 or 03
+        - QR-IBAN
+        """
+        # acc_type can be bank for isrb
+        if self.acc_type in ['bank', 'postal'] and self.l10n_ch_postal:
+            return self.l10n_ch_postal[:2] in ['01', '03']
+        return self.acc_type == 'iban' and _is_l10n_ch_qr_iban(self.acc_number)
 
     @api.onchange('acc_number')
     def _onchange_set_l10n_ch_postal(self):
