6cd47dfdd65f4a6058f087fb4c28a104070d471d
Jacques-Etienne Baudoux <je@bcim.be>
2021-08-03 16:38:29 +0200

    Temp fix commit eec0b80756557ecd543a50f25b8f63f94e23de7c that always round total

--- addons/account/models/account.py
+++ addons/account/models/account.py
@@ -1614,8 +1614,10 @@ class AccountTax(models.Model):
         # precision of the currency.
         # The context key 'round' allows to force the standard behavior.
         round_tax = False if company.tax_calculation_rounding_method == 'round_globally' else True
+        round_total = True
         if 'round' in self.env.context:
             round_tax = bool(self.env.context['round'])
+            round_total = bool(self.env.context['round'])
 
         if not round_tax:
             prec *= 1e-5
@@ -1669,7 +1671,10 @@ class AccountTax(models.Model):
         #   Line 2: sum(taxes) = 10920 - 2176 = 8744
         #   amount_tax = 4311 + 8744 = 13055
         #   amount_total = 31865 + 13055 = 37920
-        base = currency.round(price_unit * quantity)
+        if round_total:
+            base = currency.round(price_unit * quantity)
+        else:
+            base = round(price_unit * quantity, precision_rounding=prec)
 
         # For the computation of move lines, we could have a negative base value.
         # In this case, compute all with positive values and negate them at the end.
@@ -1725,7 +1730,7 @@ class AccountTax(models.Model):
                         store_included_tax_total = False
                 i -= 1
 
-        total_excluded = currency.round(recompute_base(base, incl_fixed_amount, incl_percent_amount, incl_division_amount))
+        total_excluded = recompute_base(base, incl_fixed_amount, incl_percent_amount, incl_division_amount)
 
         # 5) Iterate the taxes in the sequence order to compute missing tax amounts.
         # Start the computation of accumulated amounts at the total_excluded value.
@@ -1813,9 +1818,9 @@ class AccountTax(models.Model):
         return {
             'base_tags': taxes.mapped(is_refund and 'refund_repartition_line_ids' or 'invoice_repartition_line_ids').filtered(lambda x: x.repartition_type == 'base').mapped('tag_ids').ids,
             'taxes': taxes_vals,
-            'total_excluded': sign * total_excluded,
-            'total_included': sign * currency.round(total_included),
-            'total_void': sign * currency.round(total_void),
+            'total_excluded': sign * (currency.round(total_excluded) if round_total else total_excluded),
+            'total_included': sign * (currency.round(total_included) if round_total else total_included),
+            'total_void': sign * (currency.round(total_void) if round_total else total_void),
         }
 
     @api.model
